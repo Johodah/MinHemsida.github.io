482395b016884b40106624fd5626a99f67fe2989
Johodah <johnny.dahle@hotmail.com>
2023-11-03 10:50:47 +0100

    Lagt till en snyggare klocka och tagit bort bollen

--- funktioner.js
+++ funktioner.js
@@ -1,7 +1,27 @@
-function InstaLuring(){
-    return alert("Jag har inte Instagram!");
-}
+function startTime(){
+    const timeElement = document.getElementById("time");
+    const date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    let y = date.getFullYear();
+    let Mo = date.getMonth();
+    let d = date.getDate();
 
-const buttonF = document.getElementById("buttonF");
+    m = checkTime(m);
+    s = checkTime(s);
 
-buttonF.addEventListener('click', InstaLuring);
+    timeElement.textContent = y + "/" + Mo + "/" + d + "/" +  checkTime(h) + ":" + checkTime(m) + ":" + checkTime(s);
+    
+    setTimeout(startTime, 1000);  
+    }
+    
+    function checkTime(i){
+    if (i < 10){
+        return "0" + i;}
+        else {
+            return i;
+        }
+    }
+    
+    startTime();
